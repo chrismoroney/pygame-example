60ffb3d6d89834ba44360867bce3cbadeb6e3c73
chrismoroney <christopher.moroney1@gmail.com>
2023-08-05 12:43:00 -0700

    intro to background/screen changes

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
 from pygame.locals import *
 
-pygame.init()
+background = [1,1,2,2,2,1] 
+screen = [0] * 6
 
-pygame.font.init()
+for i in range(6):
+    screen[i] = background[i]
+print(screen)
 
+
+
+
+playerpos = 3
+#playerpos = playerpos - 1
+screen[playerpos] = 8
+# illustrate movement with array
+print(screen)
